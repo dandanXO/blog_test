bcc954b4a49737b22b8b5fa9348573c8a0e44a69
pigpig850130@gmail.com <pigpig850130@gmail.com>
2019-04-16 19:27:44 +0800

    add pm2 for production env can minify all files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,18 +3,13 @@ module.exports = {
     name: 'API',
     script: 'server/index.js',
     //cwd:'./server',
-    args:'cross-env NODE_ENV=production',
+    //args:'cross-env NODE_ENV=production',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    //args: 'one two',
     instances: '4',
     autorestart: true,
     watch: false,
     max_memory_restart: '2G',
     env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
       NODE_ENV: 'production'
     }
   }],
